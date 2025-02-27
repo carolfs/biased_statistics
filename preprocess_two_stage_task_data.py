94d4338f6c6ebb4cf89ef341170239e9bcbd3e1c
Carolina Feher da Silva <carolfsu@gmail.com>
2025-02-27 14:03:47 +0000

    Deleted commented code from preprocess_two_stage_task_data.py

--- preprocess_two_stage_task_data.py
+++ preprocess_two_stage_task_data.py
@@ -20,24 +20,4 @@ while True:
         break
 df = pd.DataFrame(df)
 df = df[df.abort == 0] # Eliminate aborted trials
-# # Select only participants with both sessions
-# subj = []
-# for s, sdf in df.groupby("subj"):
-#     if 1 in sdf.meas.unique() and 3 in sdf.meas.unique():
-#         subj.append(s)
-# df = df[df.subj.isin(subj)]
-# newkeys = ["subject", "measurment", "trial", "choice1", "choice2", "RT1", "RT2", "reward", "transition", "second_stage_state"]
-# df["subject"] = df["subj"]
-# df["measurment"] = df["meas"]
-# df["trial"] = df["trl"]
-# df["choice1"] = df["ch1"]
-# df["choice2"] = df["ch2"]
-# df["RT1"] = df["rt1"]
-# df["RT2"] = df["rt2"]
-# df["reward"] = df["rw"]
-# df["transition"] = df["tran"]
-# df["second_stage_state"] = df["state"]
-# for col in df.columns:
-#     if col not in newkeys:
-        # del df[col]
 df.to_csv("two_stage_task_data.csv", index=False)
